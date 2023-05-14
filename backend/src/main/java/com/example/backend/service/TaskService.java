6491f2d7f6307f43888006cd960fa249897236bc
Piotr Tchorzewski <piotr.tchorzewski@connectpoint.pl>
2023-05-14 19:23:18 +0200

    update TaskService

--- backend/src/main/java/com/example/backend/service/TaskService.java
+++ backend/src/main/java/com/example/backend/service/TaskService.java
@@ -1,7 +1,22 @@
 package com.example.backend.service;
 
+import com.example.backend.model.Task;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface TaskService {
 
     void deleteTaskByProjectId(Long id);
+
+    void deleteTaskByStatusNameAndProject(Long projectId, String statusName);
+
+    void deleteTask(Long id);
+
+    Optional<Task> findTaskById(Long id);
+
+    List<Task> findTasksByProjectAndStatus(Long projectId, String statusName);
+
+    Task saveTask(Task task);
 
 }
--- backend/src/main/java/com/example/backend/service/impl/TaskServiceImpl.java
+++ backend/src/main/java/com/example/backend/service/impl/TaskServiceImpl.java
@@ -1,9 +1,13 @@
 package com.example.backend.service.impl;
 
+import com.example.backend.model.Task;
 import com.example.backend.repository.TaskRepository;
 import com.example.backend.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TaskServiceImpl implements TaskService {
@@ -17,5 +21,30 @@ public class TaskServiceImpl implements TaskService {
     @Override
     public void deleteTaskByProjectId(Long id) {
         taskRepository.deleteByProjectId(id);
+    }
+
+    @Override
+    public void deleteTaskByStatusNameAndProject(Long projectId, String statusName) {
+        taskRepository.deleteByProjectIdAndStatusName(projectId, statusName);
+    }
+
+    @Override
+    public void deleteTask(Long id) {
+        taskRepository.deleteById(id);
+    }
+
+    @Override
+    public Optional<Task> findTaskById(Long id) {
+        return taskRepository.findById(id);
+    }
+
+    @Override
+    public List<Task> findTasksByProjectAndStatus(Long projectId, String statusName) {
+        return taskRepository.findByProjectIdAndStatusName(projectId, statusName);
+    }
+
+    @Override
+    public Task saveTask(Task task) {
+        return taskRepository.save(task);
     }
 }
