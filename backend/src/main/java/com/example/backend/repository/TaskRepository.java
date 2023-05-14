127f20dcc4752e03b08a8f89835050702e202321
Piotr Tchorzewski <piotr.tchorzewski@connectpoint.pl>
2023-05-14 19:23:39 +0200

    update taskRepository

--- backend/src/main/java/com/example/backend/repository/TaskRepository.java
+++ backend/src/main/java/com/example/backend/repository/TaskRepository.java
@@ -2,13 +2,25 @@ package com.example.backend.repository;
 
 import com.example.backend.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
 
     @Transactional
     void deleteByProjectId(Long projectId);
+
+    @Transactional
+    void deleteByProjectIdAndStatusName(Long projectId, String statusName);
+
+    List<Task> findByProjectIdAndStatusName(Long projectId, String statusName);
+
+    @Query("SELECT DISTINCT t.statusName FROM Task t WHERE t.project.id = :projectId")
+    List<String> findUniqueStatusesByProjectId(@Param("projectId") Long projectId);
 
 }
