b83e4202acd79efe46a8c68251895b1d70d3ac1a
Piotr Tchorzewski <piotr.tchorzewski@connectpoint.pl>
2023-05-15 22:28:58 +0200

    add user request

--- frontend/src/services/user.service.js
+++ frontend/src/services/user.service.js
@@ -8,6 +8,10 @@ class UserService {
     changeUserInfo(id, user) {
         return axios.put(API_URL + '/user/' + id, user, {headers: authHeader()});
     }
+
+    getAllUsers(id) {
+        return axios.get('http://localhost:8080/api/usersList' + id, {headers: authHeader()})
+    }
 }
 
 export default new UserService();
