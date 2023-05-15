729df35f16e7d11351263faee9dd82defe594c2f
Piotr Tchorzewski <piotr.tchorzewski@connectpoint.pl>
2023-05-15 22:27:56 +0200

    create task service

---
+++ frontend/src/services/task.service.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+import {authHeader} from "./base.service";
+
+const API_URL = 'http://localhost:8080/api/task';
+
+class TaskService {
+
+    editTask(projectId, taskId, task) {
+        return axios.put(API_URL + '/update/' + projectId + '/' + taskId, task, {headers: authHeader()})
+    }
+
+    addTask(projectId, task) {
+        return axios.post(API_URL + '/add/' + projectId, task, {headers: authHeader()})
+    }
+}
+
+export default new TaskService();
