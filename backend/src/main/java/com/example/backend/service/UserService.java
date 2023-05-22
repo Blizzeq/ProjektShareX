1664ad20efb55f1fdd192b6f2320e7fae47be94c
Piotr Tchorzewski <piotr.tchorzewski@connectpoint.pl>
2023-05-22 16:34:30 +0200

    change findById function by delete Optional

--- backend/src/main/java/com/example/backend/service/UserService.java
+++ backend/src/main/java/com/example/backend/service/UserService.java
@@ -14,7 +14,7 @@ public interface UserService {
     Optional<User> findByEmail(String email);
     Optional<User> findByUsername(String username);
 
-    Optional<User> findById(Long id);
+    User findById(Long id);
 
     List<String> usernameList(Long userId);
 
