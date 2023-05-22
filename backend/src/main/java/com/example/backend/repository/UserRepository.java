87a8019675143fb6cb7ed73ce0472312ce1abaf2
Piotr Tchorzewski <piotr.tchorzewski@connectpoint.pl>
2023-05-22 16:33:44 +0200

    add findById function

--- backend/src/main/java/com/example/backend/repository/UserRepository.java
+++ backend/src/main/java/com/example/backend/repository/UserRepository.java
@@ -1,11 +1,8 @@
 package com.example.backend.repository;
 
-import com.example.backend.model.Task;
 import com.example.backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -18,7 +15,7 @@ public interface UserRepository extends JpaRepository<User, String> {
 
     Optional<User> findByUsername(String username);
 
-    Optional<User> findById(Long id);
+    User findById(Long id);
 
     @Query("SELECT u.username FROM User u WHERE u.id <> :user_id")
     List<String> findUsernameList(@Param("user_id") Long user_id);
