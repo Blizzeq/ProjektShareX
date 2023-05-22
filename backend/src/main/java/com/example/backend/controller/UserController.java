751979d71c2ab97376bf06bbcfeb96aba4b5cd41
Piotr Tchorzewski <piotr.tchorzewski@connectpoint.pl>
2023-05-22 19:53:17 +0200

    add method addUserToProject

--- backend/src/main/java/com/example/backend/controller/UserController.java
+++ backend/src/main/java/com/example/backend/controller/UserController.java
@@ -1,12 +1,15 @@
 package com.example.backend.controller;
 
+import com.example.backend.model.Project;
+import com.example.backend.model.User;
+import com.example.backend.service.ProjectService;
 import com.example.backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/usersList")
@@ -14,8 +17,11 @@ public class UserController {
 
     private final UserService userService;
 
-    public UserController(UserService userService) {
+    private final ProjectService projectService;
+
+    public UserController(UserService userService, ProjectService projectService) {
         this.userService = userService;
+        this.projectService = projectService;
     }
 
     @GetMapping("{userId}")
@@ -23,4 +29,30 @@ public class UserController {
         return ResponseEntity.ok(userService.usernameList(userId));
     }
 
+    @GetMapping("{projectId}/unassigned-users")
+    public ResponseEntity<List<User>> getUnassignedUsers(@PathVariable Long projectId) {
+        List<User> unassignedUsers = userService.findUnassignedUsers(projectId);
+
+        return ResponseEntity.ok(unassignedUsers);
+    }
+
+    @PostMapping("add/{projectId}/{userId}")
+    public ResponseEntity<String> addUserToProject(@PathVariable Long userId, @PathVariable Long projectId) {
+        User user = userService.findById(userId);
+        Optional<Project> projectOptional = projectService.findProjectById(projectId);
+
+        if (projectOptional.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        Project project = projectOptional.get();
+
+        user.getProjects().add(project);
+        project.getUsers().add(user);
+
+        userService.saveUser(user);
+        projectService.saveProject(project);
+
+        return ResponseEntity.ok("User added to project successfully.");
+    }
 }
