27144f5d625a74383f3d3b20ab35cfb94760056c
Piotr Tchorzewski <piotr.tchorzewski@connectpoint.pl>
2023-06-05 12:23:11 +0200

    add setNewStatus(null)

--- frontend/src/Components/Modal/EditStatusModal.js
+++ frontend/src/Components/Modal/EditStatusModal.js
@@ -14,6 +14,7 @@ const EditStatusModal = ({status, setNewStatus, onClose, onSubmit}) => {
         validationSchema: editStatusModalSchema,
         onSubmit: (values) => {
             handleSubmit(values);
+            setNewStatus(null);
         },
     });
 
