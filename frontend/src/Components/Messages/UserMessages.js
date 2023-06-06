4381bed3c2908d268a8766a4a6a4313576902fbc
Piotr Tchorzewski <piotr.tchorzewski@connectpoint.pl>
2023-06-06 11:21:41 +0200

    change static list to request from backend

--- frontend/src/Components/Messages/UserMessages.js
+++ frontend/src/Components/Messages/UserMessages.js
@@ -1,28 +1,61 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Header from "../Header/Header";
-import {LinkContainer} from "react-router-bootstrap";
 import "./UserMessages.css";
+import {useSelector} from "react-redux";
+import NotificationService from "../../services/notification.service";
 
 function UserMessages(props) {
 
-    const messages = [
-        "Your project 'nazwa' has been deleted.",
-        "New message received.",
-        "Task completed successfully.",
-        // Dodaj inne przykładowe wiadomości tutaj
-    ];
+    const currentUser = useSelector((state) => state.user);
+    const [notifications, setNotifications] = useState(null);
+    const [headerKey, setHeaderKey] = useState(0);
+
+    const fetchNotifications = async () => {
+        try {
+            const response = await NotificationService.getAllNotificationsOfUser(currentUser.id);
+            setNotifications(response.data);
+        } catch (error) {
+            console.error('Error while fetching notifications:', error);
+        }
+    };
+
+    const refreshHeader = () => {
+        setHeaderKey((prevKey) => prevKey + 1);
+    };
+
+    const handleClick = async (notificationId) => {
+        try {
+            await NotificationService.changeIsReadToTrue(notificationId);
+            fetchNotifications();
+            refreshHeader();
+        } catch (error) {
+            console.error('Error while changing is read to true:', error);
+        }
+    };
+
+    useEffect(() => {
+        NotificationService.getAllNotificationsOfUser(currentUser.id).then((response) => {
+            console.log(response.data);
+            setNotifications(response.data);
+        });
+    }, []);
 
     return (
     <div className={'home Regular right'}>
-        <Header />
+        <Header key={headerKey} />
         <hr></hr>
         <div className={'flex h-full items-center justify-center message-content'}>
             <div className={'messages'}>
-                {messages.map((message, index) => (
-                    <div className={'delete-message'} key={index}>
-                        {message}
-                    </div>
-                ))}
+                {notifications !== null &&
+                    notifications.map((notification) => (
+                        <button
+                            className={`delete-message ${!notification.read ? 'unread' : ''}`}
+                            key={notification.id}
+                            onClick={() => handleClick(notification.id)}
+                        >
+                            {notification.description}
+                        </button>
+                    ))}
             </div>
         </div>
     </div>
