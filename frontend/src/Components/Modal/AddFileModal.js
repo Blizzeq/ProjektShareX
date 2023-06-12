8086d02a165ab62d2f116ab1d25c24fe7fa177a2
Piotr Tchorzewski <piotr.tchorzewski@connectpoint.pl>
2023-06-12 20:31:02 +0200

    add show and delete method to modal

--- frontend/src/Components/Modal/AddFileModal.js
+++ frontend/src/Components/Modal/AddFileModal.js
@@ -1,9 +1,12 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import { Form } from 'react-bootstrap';
+import {useSelector} from "react-redux";
+import FileService from "../../services/file.service";
+import deletefile from "../../Assets/Home/MinusCircleRed.svg";
 
 function AddFileModal({ files, setFiles, closeModal, taskId }) {
-    const [isFileSaved, setIsFileSaved] = useState(false);
-    const [selectedFileName, setSelectedFileName] = useState('');
+    const currentUser = useSelector((state) => state.user);
+    const [fileList, setFileList] = useState([]);
 
     const handleFileChange = (event) => {
         const selectedFile = event.target.files[0];
@@ -11,41 +14,75 @@ function AddFileModal({ files, setFiles, closeModal, taskId }) {
             ...files,
             name: selectedFile.name,
             type: selectedFile.type,
-            data: selectedFile,
-            taskId: taskId,
+            fileData: selectedFile,
+            uploadBy: currentUser.id,
         });
     };
 
-    const handleSave = () => {
-        if (files.data) {
-            setIsFileSaved(true);
+    useEffect(() => {
+        FileService.getFilesListAssignedToTask(taskId).then((response) => {
+            console.log(response.data);
+            setFileList(response.data);
+        });
+    }, []);
+
+    const handleSave = async () => {
+        if (files.fileData) {
+            const formData = new FormData();
+            formData.append('file', files.fileData);
+
+            await FileService.uploadFile(formData, taskId)
+
+            FileService.getFilesListAssignedToTask(taskId).then((response) => {
+                console.log(response.data);
+                setFileList(response.data);
+            });
+
             console.log('Plik zapisany:', files);
         }
     };
 
-    const handleFileDownload = () => {
-        const downloadLink = document.createElement('a');
-        downloadLink.href = URL.createObjectURL(files.data);
-        downloadLink.download = files.name;
-        downloadLink.click();
+    const handleFileDownload = async (fileName) => {
+        try {
+            const response = await FileService.downloadFile(fileName);
+            const url = window.URL.createObjectURL(new Blob([response.data]));
+
+            const link = document.createElement('a');
+            link.href = url;
+            link.setAttribute('download', fileName);
+            document.body.appendChild(link);
+            link.click();
+            link.remove();
+            window.URL.revokeObjectURL(url);
+        } catch (error) {
+            console.log('Błąd podczas pobierania pliku:', error);
+        }
     };
+
+    const handleDeleteFile = async (fileId) => {
+        await FileService.deleteAssignedFileToTask(fileId);
+
+        FileService.getFilesListAssignedToTask(taskId).then((response) => {
+            console.log(response.data);
+            setFileList(response.data);
+        });
+    }
 
     return (
         <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center">
             <div className="bg-white p-8 rounded">
                 <h2 className="text-xl font-bold mb-4">Task Files</h2>
                 <div className="flex flex-col delete-confirmation-content gap-2">
-                    {files && files.data ? (
-                        <div>
-                            <a href={URL.createObjectURL(files.data)} download={files.name}>
-                                <p>{files.name}</p>
-                            </a>
+                    {fileList.map((item) => (
+                        <div key={item.id} className={'flex'}>
+                            <p>
+                                <button onClick={() => handleFileDownload(item.name)}>{item.name}</button>
+                            </p>
+                            <button onClick={() => handleDeleteFile(item.id)}>
+                                <img src={deletefile} alt={'delete-file'} className={'w-5 ml-1'}/>
+                            </button>
                         </div>
-                    ) : (
-                        <div>
-                            <p>{files.name}</p>
-                        </div>
-                    )}
+                    ))}
                     <div className="delete-confirmation-buttons">
                         <Form.Group controlId="formFileMultiple" className="mb-3" onChange={handleFileChange}>
                             <h3 className={'mb-3 font-bold'}>Add new files</h3>
@@ -60,7 +97,6 @@ function AddFileModal({ files, setFiles, closeModal, taskId }) {
                             Cancel
                         </button>
                     </div>
-                    {isFileSaved && <div><p>File has been saved</p></div>}
                 </div>
             </div>
         </div>
